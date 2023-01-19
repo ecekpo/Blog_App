51e0c943aba4870fdf36e6ede8b3c6c2da1fee30
Emem Ekpo <ememekpo@yahoo.com>
2023-01-19 12:00:10 +0100

    Add method to return the most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
+
+  def return_most_recent_post
+    posts.last(3)
+  end
 end
