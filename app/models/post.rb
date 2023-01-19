f4d3294663830c739815119b8217b5a31cd25c32
Emem Ekpo <ememekpo@yahoo.com>
2023-01-19 12:05:05 +0100

    Add posts counter update

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :comments
+
+  after_create :post_counter_update
+
+  def post_counter_update
+    author.increment!(:posts_counter)
+  end
 end
