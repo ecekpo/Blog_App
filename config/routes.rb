2204e25011fba25216404996620c8cd10c6be17c
Emem Ekpo <ememekpo@yahoo.com>
2023-01-25 16:25:23 +0100

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  resources :users do
-    resources :posts do
-          resources :likes
-          resources :comments
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
     end
   end
 end
