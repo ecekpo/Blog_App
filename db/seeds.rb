eb5dd1d241eadf266c5a28ad8b26c3cb482634dd
Emem Ekpo <ememekpo@yahoo.com>
2023-01-25 17:06:23 +0100

    Add faker file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+user_one = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+
+user_two = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+
+user_three = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+
+user_four = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+
+user_five = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
