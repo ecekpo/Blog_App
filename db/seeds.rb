2488d31111ba04c36ea0a37a20a04242f075015c
Emem Ekpo <ememekpo@yahoo.com>
2023-01-26 16:37:33 +0100

    Add views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 require 'faker'
 
-user_one = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+user_one = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6))
 
-user_two = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+user_two = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6))
 
-user_three = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+user_three = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6))
 
-user_four = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+user_four = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6))
 
-user_five = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(slug: "user-#{i+1}", size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6)
+user_five = User.create(name: Faker::Name.name, photo: Faker::Avatar.image(size: "300x300", format: "jpg", set: "set1", bgset: "bg1"), bio: Faker::Lorem.paragraph(sentence_count: 6))
 
 post_one = Post.create(author: user_one, title: Faker::Lorem.paragraph(sentence_count: 1), text: Faker::Lorem.paragraph(sentence_count: 40))
 
