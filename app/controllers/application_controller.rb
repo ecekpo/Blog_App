3f2f9879af915ba0272240421ea4146be06c70db
Emem Ekpo <ememekpo@yahoo.com>
2023-02-07 19:04:41 +0100

    add modify application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit({ roles: [] }, :email, :password, :password_confirmation,:name, :bio, :photo, :post_counter)
+      user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :name, :bio, :photo, :post_counter)
     end
   end
 end
